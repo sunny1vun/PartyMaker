edd259fc69d8da4686287587a464e77fcbf91d79
Tony Star <sunn1vun@gmail.com>
2016-02-05 13:34:48 +0200

    prelast commit

--- PartyMaker/SUNMakingPartyVC.m
+++ PartyMaker/SUNMakingPartyVC.m
@@ -37,8 +37,23 @@
 //    [self.navigationController popToRootViewControllerAnimated:YES];
     
 }
+#pragma mark- Making UnclickAbleFronend
 
-#pragma mark- Making PartyMakerView
+-(void)addRound:(NSString*) forLabel{
+    if ([forLabel isEqualToString:@"CHOOSE DAY"]) {
+        UIView *round= [[UIView alloc] initWithFrame:CGRectMake(10.f, 139.5f, 10.f, 10.f)];
+        //    UIView *dateView= [[UIView alloc] initWithFrame:CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)];
+//        UILabel *labelOfRound = [[UILabel alloc] initWithFrame:(CGRect){}];
+    }
+//    else if(){}
+
+}
+
+-(void)addLine{
+    
+}
+
+#pragma mark- Making ClickableFrontend
 
 -(void)addDateButton{
     UIButton *chooseDate= [UIButton buttonWithType:UIButtonTypeCustom];
@@ -59,6 +74,7 @@
     textField.placeholder = @"Your Party Name";
     NSAttributedString *str = [[NSAttributedString alloc] initWithString:@"Some Text" attributes:@{ NSForegroundColorAttributeName : [[UIColor alloc] initWithRed:76/255.f green:82/255.f blue:92/255.f alpha:1.f] }];
     textField.attributedPlaceholder = str;
+
     textField.textColor = [UIColor whiteColor];
     textField.font = [UIFont fontWithName:@"Helvetica" size:14];
     textField.backgroundColor = [[UIColor alloc] initWithRed:35/255.f green:37/255.f blue:43/255.f alpha:1.f];
@@ -158,6 +174,8 @@
     [self.view addSubview:chooseCancel];
 }
 
+#pragma mark- Making Backend
+
 #pragma mark- Working with PartyMakerView
 -(void)makeParty{
     
@@ -165,6 +183,7 @@
     partyMakerVC.view = [[UIView alloc] initWithFrame:self.view.frame];
 //    UIView *dateView= [[UIView alloc] initWithFrame:CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)];
     [partyMakerVC.navigationItem setHidesBackButton:YES];
+    [partyMakerVC addRound:@"CHOOSE DAY"];
     [partyMakerVC addDateButton];
     [partyMakerVC addTextField];
     [partyMakerVC addTopSlider];
@@ -174,88 +193,6 @@
     [partyMakerVC addSaveButton];
     [partyMakerVC addCancelButton];
     
-    
-    /*
-    //от сюда
-    UIButton *chooseDate= [UIButton buttonWithType:UIButtonTypeCustom];
-    chooseDate.frame= (CGRect){121, 74, 190, 37};
-    chooseDate.layer.cornerRadius= 3.f;
-    chooseDate.backgroundColor= [[UIColor alloc] initWithRed:239/255.f green:177/255.f blue:27/255.f alpha:1.f];
-    [chooseDate setTitle:@"Normal"forState:UIControlStateNormal];
-    [chooseDate setTitle:@"Highlighted"forState:UIControlStateHighlighted];
-    [chooseDate setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-    [chooseDate addTarget:partyMakerVC action:@selector(onButtonClicked:) forControlEvents:UIControlEventTouchUpInside];
-    [partyMakerVC.view addSubview:chooseDate];
-    
-    
-    UITextField *textField = [[UITextField alloc] initWithFrame:(CGRect){121, 121, 190, 37}];
-    textField.layer.cornerRadius= 3.f;
-    textField.placeholder = @"Your Party Name";
-    NSAttributedString *str = [[NSAttributedString alloc] initWithString:@"Some Text" attributes:@{ NSForegroundColorAttributeName : [[UIColor alloc] initWithRed:76/255.f green:82/255.f blue:92/255.f alpha:1.f] }];
-    textField.attributedPlaceholder = str;
-    textField.textColor = [UIColor whiteColor];
-    textField.font = [UIFont fontWithName:@"Helvetica" size:14];
-    textField.backgroundColor = [[UIColor alloc] initWithRed:35/255.f green:37/255.f blue:43/255.f alpha:1.f];
-    textField.clearButtonMode = UITextFieldViewModeWhileEditing;
-    textField.rightViewMode = UITextFieldViewModeAlways;
-//    textField.rightView = [[UIImageViewalloc] initWithImage:[UIImageimageNamed:@"Img1"]];
-    textField.leftViewMode = UITextFieldViewModeAlways;
-//    textField.leftView = [[UIImageView alloc] ...];
-    textField.returnKeyType = UIReturnKeyDone;
-//    textField.delegate = partyMakerVC;
-    [textField addTarget:partyMakerVC action:@selector(onTextFieldEditingEnded) forControlEvents:UIControlEventEditingDidEnd];
-    [partyMakerVC.view addSubview:textField];
-    
-    
-    UISlider *slider = [[UISlider alloc] initWithFrame:(CGRect){174, 174, 137, 30}];
-    slider.minimumValueImage = [UIImage imageNamed:@"Sun-32"];
-    slider.maximumValueImage = [[UIImage imageNamed:@"Sun-64"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-    slider.minimumTrackTintColor = [[UIColor alloc] initWithRed:240/255.f green:180/255.f blue:30/255.f alpha:1.f];
-    slider.maximumTrackTintColor = [[UIColor alloc] initWithRed:28/255.f green:30/255.f blue:35/255.f alpha:1.f];
-    slider.thumbTintColor = [UIColor whiteColor];
-    slider.tintColor = [UIColor blackColor];
-    slider.value = 0.3;
-    [slider addTarget:partyMakerVC action:@selector(onSlide:) forControlEvents:UIControlEventValueChanged];
-    [partyMakerVC.view addSubview:slider];
-    
-    
-    UISlider *sliderBot = [[UISlider alloc] initWithFrame:(CGRect){121, 229, 137, 30}];
-    sliderBot.minimumValueImage = [UIImage imageNamed:@"Sun-32"];
-    sliderBot.maximumValueImage = [[UIImage imageNamed:@"Sun-64"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-    sliderBot.minimumTrackTintColor = [[UIColor alloc] initWithRed:240/255.f green:180/255.f blue:30/255.f alpha:1.f];
-    sliderBot.maximumTrackTintColor = [[UIColor alloc] initWithRed:28/255.f green:30/255.f blue:35/255.f alpha:1.f];
-    sliderBot.thumbTintColor = [UIColor whiteColor];
-    sliderBot.tintColor = [UIColor blackColor];
-    sliderBot.value = 0.3;
-    [sliderBot addTarget:partyMakerVC action:@selector(onSlide:) forControlEvents:UIControlEventValueChanged];
-    [partyMakerVC.view addSubview:sliderBot];
-    
-    
-    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:(CGRect){121, 289, 190, 101}];
-    scrollView.pagingEnabled = YES;
-    scrollView.scrollIndicatorInsets = UIEdgeInsetsMake(0, 0, 55, 0);
-    UIImageView *imageView = [[UIImageView alloc] init];
-    [scrollView.viewForFirstBaselineLayout setBackgroundColor:[UIColor grayColor]];
-    [scrollView addSubview:imageView];
-    scrollView.contentSize = imageView.frame.size;
-    UIPageControl *pageControl = [[UIPageControl alloc] initWithFrame:(CGRect){0, 1284, 750, 50}];
-    pageControl.numberOfPages = 5;
-    pageControl.currentPage = 0;
-    pageControl.backgroundColor = [UIColor blackColor];
-    [pageControl addTarget:partyMakerVC action:@selector(onPageChanged:) forControlEvents:UIControlEventValueChanged];
-    
-//    pageControl.numberOfPages = scrollView.contentSize.width/[UIScreenmainScreen].bounds.size.width;[partyMakerVC.view addSubview:pageControl];
-//    partyMakerVC.pageControl = pageControl;
-//    UIView *viewForSliderImages= [[UIView alloc] initWithFrame:(CGRect){121, 229, 190, 101}];
-//    viewForSliderImages.scrollView = scrollView;
-//    [viewForSliderImages addSubview:pageControl];
-    [partyMakerVC.view addSubview:scrollView];
-    [partyMakerVC.view addSubview:pageControl];
-    
-    partyMakerVC.pageControl = pageControl;
-    partyMakerVC.scrollView = scrollView;
-    
-    // до сюда*/
     
     [self.navigationController pushViewController:partyMakerVC animated:YES];
 }
